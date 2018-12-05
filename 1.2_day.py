d22e0b36a25a6b233546c64414d000381f1cf6c8
SonnySkyez <hnic85@gmail.com>
2018-12-05 16:42:43 +0100

    Commit 2 - 05.12

--- 1.2_day.py
+++ 1.2_day.py
@@ -27,31 +27,25 @@ Starting with a frequency of zero, what is the resulting frequency after all of 
 in frequency have been applied?
 '''
 
-# PART 2
 
 res_frequency = 0   # starting with frequency 0
 frequencies = []    # an empty list of frequency results
 reached_twice = False
 
-# open file:
 
-
-
-while reached_twice is False:   # loop trouth the file untill result is reached twice;
+while reached_twice is False:   # loop trough the file until result is reached twice
 
     # open proper file:
     with open('/Users/chrisbrown/PycharmProjects/Advent_of_code/1_input.txt', 'r') as file:
 
-
         for line in file:
 
                 res_frequency += int(line)  # change line to integer and add to result frequency
-                print(res_frequency)
 
 
                 if res_frequency in frequencies:
 
-                    print('First frequency reached twice is: ',res_frequency)
+                    print('First frequency reached twice is: ', res_frequency)
                     reached_twice = True
                     break
 
